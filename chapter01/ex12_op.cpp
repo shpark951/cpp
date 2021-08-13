92a2ee5db9c92732512ada2c7d1853f5539e9fe0
shpark951 <69614932+shpark951@users.noreply.github.com>
2021-08-13 15:53:41 +0900

    chpa

---
+++ chapter01/ex12_op.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char const *argv[]) {
+ int x = 100;
+ int y = 200;
+ int result = x + y;
+ cout << "x + y : " << result << endl;
+ cout << "x - y : " << x- y << endl;
+ cout << "x / y : " << x /y << endl;
+ cout << "x % 3 : " << x % 3 << endl;
+ return 0; 
+ }
